74ebab4463572264e38a48082faa6441c89f7176
MOHAMMAD RIZWANULLAH <78429736+rizwanrockzz@users.noreply.github.com>
2022-01-01 13:06:16 +0530

    Create normal.py

---
+++ normal.py
@@ -0,0 +1,29 @@
+# years 
+# money
+# totalmoney abd returs = 10%
+
+# NORMAL INTREST LOGIC
+
+years = int(input("Total no of years : "))
+initial = int(input("Initial amount : "))
+returns = int(input("Enter the return for a year : "))
+total = initial
+t = years
+i = years
+while(i!=0):
+    intrest = returns/100
+    print("Intrest is : ",intrest)
+    intrestAmount = initial*intrest
+    
+    total =total+intrestAmount
+    print("The intrest is : ",intrestAmount)
+    print("In {} year total is : {} ".format(i,total))
+    i = i-1
+    # initial = total
+    
+print("The initial : ",initial)
+print("After {} years total is : {}".format(years,total))
+    
+    
+    
+    
